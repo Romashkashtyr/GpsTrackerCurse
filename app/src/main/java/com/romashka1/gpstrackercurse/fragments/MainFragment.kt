91f9e8907651ddbf4cb60d860082099d5ce5c3aa
Romashkashtyr <Romainshtyr@yandex.ru>
2024-02-09 15:46:25 +0300

    Adding OSM

--- app/src/main/java/com/romashka1/gpstrackercurse/fragments/MainFragment.kt
+++ app/src/main/java/com/romashka1/gpstrackercurse/fragments/MainFragment.kt
@@ -1,11 +1,18 @@
 package com.romashka1.gpstrackercurse.fragments
 
+import android.content.Context
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import com.romashka1.gpstrackercurse.databinding.FragmentMainBinding
+import org.osmdroid.config.Configuration
+import org.osmdroid.library.BuildConfig
+import org.osmdroid.util.GeoPoint
+import org.osmdroid.views.overlay.mylocation.GpsMyLocationProvider
+import org.osmdroid.views.overlay.mylocation.MyLocationNewOverlay
 
 
 class MainFragment : Fragment() {
@@ -15,8 +22,34 @@ class MainFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
+        settingsOsm()
         binding = FragmentMainBinding.inflate(inflater, container, false)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        initOSM()
+    }
+
+    private fun settingsOsm(){
+        Configuration.getInstance().load(
+            activity as AppCompatActivity,
+        activity?.getSharedPreferences("osm_pref", Context.MODE_PRIVATE)
+        )
+        Configuration.getInstance().userAgentValue = BuildConfig.APPLICATION_ID
+    }
+
+    private fun initOSM() = with(binding){
+        map.controller.setZoom(20.0)
+        val mLocProvider = GpsMyLocationProvider(activity)
+        val mLocOverLay = MyLocationNewOverlay(mLocProvider, map)
+        mLocOverLay.enableMyLocation()
+        mLocOverLay.enableFollowLocation()
+        mLocOverLay.runOnFirstFix {
+            map.overlays.clear()
+            map.overlays.add(mLocOverLay)
+        }
     }
 
     companion object {
--- app/src/main/java/com/romashka1/gpstrackercurse/utils/Extensions.kt
+++ app/src/main/java/com/romashka1/gpstrackercurse/utils/Extensions.kt
@@ -1,5 +1,6 @@
 package com.romashka1.gpstrackercurse.utils
 
+import android.util.Log
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
@@ -13,12 +14,16 @@ fun Fragment.openFragment(f: Fragment){
         .commit()
 }
 
-fun AppCompatActivity.openFragment(f: Fragment){
-    supportFragmentManager
-        .beginTransaction()
-        .setCustomAnimations(android.R.anim.fade_in,android.R.anim.fade_out)
-        .replace(R.id.placeHolder, f)
-        .commit()
+fun AppCompatActivity.openFragment(f: Fragment) {
+    if (supportFragmentManager.fragments.isNotEmpty()) {
+        if(supportFragmentManager.fragments[0].javaClass == f.javaClass) return
+    }
+        supportFragmentManager
+            .beginTransaction()
+            .setCustomAnimations(android.R.anim.fade_in, android.R.anim.fade_out)
+            .replace(R.id.placeHolder, f)
+            .commit()
+
 }
 
 fun Fragment.showToast(s:String){
