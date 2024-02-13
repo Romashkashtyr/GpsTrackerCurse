55712bb4ad1e64632d8d1f28682f2b182adc08de
Romashkashtyr <Romainshtyr@yandex.ru>
2024-02-13 17:33:15 +0300

    Adding check permissions

--- app/src/main/java/com/romashka1/gpstrackercurse/fragments/MainFragment.kt
+++ app/src/main/java/com/romashka1/gpstrackercurse/fragments/MainFragment.kt
@@ -1,13 +1,20 @@
 package com.romashka1.gpstrackercurse.fragments
 
+import android.Manifest
 import android.content.Context
+import android.os.Build
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import com.romashka1.gpstrackercurse.databinding.FragmentMainBinding
+import com.romashka1.gpstrackercurse.utils.checkPermission
+import com.romashka1.gpstrackercurse.utils.showToast
 import org.osmdroid.config.Configuration
 import org.osmdroid.library.BuildConfig
 import org.osmdroid.util.GeoPoint
@@ -16,6 +23,7 @@ import org.osmdroid.views.overlay.mylocation.MyLocationNewOverlay
 
 
 class MainFragment : Fragment() {
+    private lateinit var pLauncher: ActivityResultLauncher<Array<String>>
     private lateinit var binding: FragmentMainBinding
 
     override fun onCreateView(
@@ -29,7 +37,8 @@ class MainFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        initOSM()
+        registerPermissions()
+        checkLockPermission()
     }
 
     private fun settingsOsm(){
@@ -52,8 +61,58 @@ class MainFragment : Fragment() {
         }
     }
 
-    companion object {
+    private fun registerPermissions(){
+        pLauncher = registerForActivityResult(
+            ActivityResultContracts.RequestMultiplePermissions()){
+            if(it[Manifest.permission.ACCESS_FINE_LOCATION] == true){
+                initOSM()
+            } else {
+                showToast("Вы не дали разрешения на использование местоположения!")
+            }
+        }
+    }
 
+    private fun checkLockPermission(){
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q){
+            checkPermissionAfter10()
+        } else {
+            checkPermissionBefore10()
+        }
+    }
+
+    @RequiresApi(Build.VERSION_CODES.Q)
+    private fun checkPermissionAfter10() {
+        if (checkPermission(Manifest.permission.ACCESS_FINE_LOCATION)
+            && checkPermission(Manifest.permission.ACCESS_BACKGROUND_LOCATION)
+        ) {
+            initOSM()
+        } else {
+            pLauncher.launch(
+                arrayOf(
+                    Manifest.permission.ACCESS_FINE_LOCATION,
+                    Manifest.permission.ACCESS_BACKGROUND_LOCATION
+                )
+            )
+        }
+
+    }
+
+
+    private fun checkPermissionBefore10() {
+        if (checkPermission(Manifest.permission.ACCESS_FINE_LOCATION)
+            && checkPermission(Manifest.permission.ACCESS_BACKGROUND_LOCATION)
+        ) {
+            initOSM()
+        } else {
+            pLauncher.launch(
+                arrayOf(Manifest.permission.ACCESS_FINE_LOCATION)
+            )
+        }
+
+    }
+
+
+    companion object {
         @JvmStatic
         fun newInstance() = MainFragment()
             }
